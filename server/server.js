e4e2c1b4072664dab2e4d82430566551fb7f00dd
Anish-kumar7641 <72142986+Anish-kumar7641@users.noreply.github.com>
2024-08-10 19:09:27 +0530

    added submit button

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const axios = require('axios');
+const { exec } = require('child_process');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -7,50 +11,6 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Express is running");
-});
-
-const runCode = async (code, languageId) => {
-    const options = {
-        method: 'POST',
-        url: 'https://judge0-ce.p.rapidapi.com/submissions',
-        headers: {
-            'content-type': 'application/json',
-            'X-RapidAPI-Key': '8dfa7886f9mshce19386e921de44p12d6bdjsnd09a81f8f7be', // Replace with your RapidAPI key
-            'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
-        },
-        data: {
-            source_code: code,
-            language_id: languageId,
-        }
-    };
-
-    const submission = await axios.request(options);
-    const { token } = submission.data;
-
-    const resultOptions = {
-        method: 'GET',
-        url: `https://judge0-ce.p.rapidapi.com/submissions/${token}`,
-        headers: {
-            'X-RapidAPI-Key': 'YOUR_RAPIDAPI_KEY', // Replace with your RapidAPI key
-            'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
-        }
-    };
-
-    const result = await axios.request(resultOptions);
-    return result.data;
-};
-
-// Define the new POST route to handle code submissions
-app.post("/api/run-code", async (req, res) => {
-    const { code, languageId } = req.body;
-    try {
-        const result = await runCode(code, languageId);
-        console.log(result);
-        res.json(result);
-    } catch (error) {
-        console.log("Hello");
-        res.status(500).json({ error: "Code execution failed", details: error.message });
-    }
 });
 
 app.listen(PORT, async (error) => {
